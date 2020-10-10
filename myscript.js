73d9ef821ba493481f81d980c2d92d92f45d7110
Pani Bhengsri <panipuck.bhengsri@tufts.edu>
2020-10-10 23:56:28 +0700

    Add HTML

---
+++ myscript.js
@@ -0,0 +1,12 @@
+
+var numHotdogs, numFries, numSodas
+
+numHotdogs = prompt("How many hotdogs?");
+
+numFries = prompt("How many fries?");
+
+numSodas = prompt("How many sodas?");
+
+alert("this is an alert");
+
+document.write("this is a document write");
